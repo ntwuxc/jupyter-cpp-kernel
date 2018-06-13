db87cdf87a615c0d9e1356f6f9cdb5dbd5b5d95b
mycode <35049539+ntwuxc@users.noreply.github.com>
2018-06-13 08:19:39 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,16 @@
 from setuptools import setup
 
 setup(name='jupyter_cpp_kernel',
-    version='0.0.2',
+    version='0.0.3',
     description='c++ kernel for Jupyter',
     author='ChunweiYan',
     author_email='yanchunwei@outlook.com',
+    license='MIT',
+    classifiers=[
+          'License :: OSI Approved :: MIT License',
+      ],
+    url='https://github.com/ntwuxc/jupyter-cpp-kernel/',
+    download_url='https://github.com/ntwuxc/jupyter-cpp-kernel/tarball/0.0.3',
     packages=['jupyter_cpp_kernel'],
     scripts=['jupyter_cpp_kernel/install_cpp_kernel'],
     keywords=['jupyter', 'notebook', 'kernel', 'c++', 'cpp']
